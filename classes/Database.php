ed0a8066d0639496913ee4d0940f3a3c32a5604f
mlp-dw <mlp.dwwb@gmail.com>
2022-04-01 11:22:24 +0200

    ml bases

---
+++ classes/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database {
+    private $db;
+
+    public function __construct() {
+        include './config/db.php';
+        $this->db = $db;
+    }
+
+    public function insertInto(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function selectOne(){
+
+    }
+
+    public function selectAll(){
+
+    }
+
+    public function selectAllWhere(){
+        
+    }
+}
---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+try
+{
+    $db = new PDO("mysql:host=127.0.0.1;dbname=comparo_simple;charset=utf8", 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
