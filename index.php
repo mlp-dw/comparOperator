2f8b237cedea1628e5414adf6fe2ad7dad3feec6
GRILLO Benjamin <94958182+GRILLO-Benjamin@users.noreply.github.com>
2022-04-04 15:17:48 +0200

    loop images

--- index.php
+++ index.php
@@ -4,22 +4,22 @@ $manager = new Manager();
 $showDestinations = $manager->getAllDestinations();
 ?>
 
-
+<a href="">
 <?php 
 foreach($showDestinations as $destination)
         {?>
 
-<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
+<div class="image" style="width: 18rem;">
+    <img src="<?=$destination->getImage();?>" class="card-img-top" alt="...">
     <div class="card-body">
-        <div class="location"><?=$destination->getLocation();?></div>
-        <div class="price"><?=$destination->getPrice();?></div>
+        <div class="location">Destination : <?=$destination->getLocation();?></div>
+        <div class="price">Prix : <?=$destination->getPrice();?></div>
 
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
 </div>
     <?php };?>
-
+</a>
 <?php include './utils/footer.php';?>
 
 
