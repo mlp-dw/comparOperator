e49eb1f74ef8d56ecb60ad9a1e6bdf75d247c628
GRILLO Benjamin <94958182+GRILLO-Benjamin@users.noreply.github.com>
2022-04-06 12:30:44 +0200

    last error

--- process/add_offer.php
+++ process/add_offer.php
@@ -1,3 +1,19 @@
 <?php
 include "../config/autoload.php";
+$manager = new Manager;
+$location = $_POST["destination"];
+$getDestination = $manager->properImage($location);
+// $image = $getDestination->getImage();
+var_dump($getDestination);
+// // GET LOCATION------------------------------
+// $data = array(
+//     'location' => $location,
+//     'price' => $_POST["price"],
+//     'image' => $image,
+//     'tour_operator_id' => $_POST["tour_operator"]
+// );
+// $offer = new Destination($data);
 
+// $manager->createDestination($offer);
+
+// header("Location: ../admin.php?success=New offer successfully added !");
