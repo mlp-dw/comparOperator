f0ce75049c8e73cd6c5f9857684295ae81d50af8
GRILLO Benjamin <94958182+GRILLO-Benjamin@users.noreply.github.com>
2022-05-09 13:49:18 +0200

    ----

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO("mysql:host=141.94.22.233;dbname=comparo_simple;charset=utf8", 'root', '');
+    $db = new PDO("mysql:host=141.94.22.233;dbname=comparo_simple;charset=utf8", 'benji', 'estala2022mierda');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
